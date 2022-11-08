5f389f7af3ad9e20e24b63763612c1209fafcb36
Eivind Morch <eivind@morch.priv.no>
2022-11-08 17:08:58 +0100

    Remove id as File property

--- src/main/java/no/fintlabs/model/File.java
+++ src/main/java/no/fintlabs/model/File.java
@@ -1,6 +1,5 @@
 package no.fintlabs.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,17 +8,12 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class File {
-
-    @NotNull
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private UUID id;
 
     @NotBlank
     private String name;
