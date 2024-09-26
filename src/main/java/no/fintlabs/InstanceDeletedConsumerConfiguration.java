62a8259e834aba2a5be20f56deb2b3a4d1bca4d8
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-09-26 09:02:40 +0200

    remove creation of instance deleted topic. this is instance-service responsibility

--- src/main/java/no/fintlabs/InstanceDeletedConsumerConfiguration.java
+++ src/main/java/no/fintlabs/InstanceDeletedConsumerConfiguration.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.flyt.kafka.event.InstanceFlowEventConsumerFactoryService;
 import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
-import no.fintlabs.kafka.event.topic.EventTopicService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
