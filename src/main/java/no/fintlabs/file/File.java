eb3c607dfdbfccd3533f1642596cec6c1c616480
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-11-05 15:35:34 +0100

    delete redundant deserializer

--- src/main/java/no/fintlabs/file/File.java
+++ src/main/java/no/fintlabs/file/File.java
@@ -1,6 +1,5 @@
 package no.fintlabs.file;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import jakarta.validation.constraints.NotBlank;
@@ -10,7 +9,6 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
-import no.fintlabs.Base64Deserializer;
 import org.springframework.http.MediaType;
 
 @Getter
@@ -34,7 +32,6 @@ public class File {
     private String encoding;
 
     @NotEmpty
-    @JsonDeserialize(using = Base64Deserializer.class)
     private byte[] contents;
 
 }
