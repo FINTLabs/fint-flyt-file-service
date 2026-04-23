12e7ca4eca3f93d174138afd56da8a92d6705d9b
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-04-23 12:55:07 +0200

    Fiks Azure-opplasting for filnavn med unicode i blob-metadata

--- src/test/kotlin/no/novari/flyt/files/domain/FilePayloadTest.kt
+++ src/test/kotlin/no/novari/flyt/files/domain/FilePayloadTest.kt
@@ -6,31 +6,6 @@ import org.springframework.http.MediaType
 
 class FilePayloadTest {
     @Test
-    fun `equals and hashCode use byte array contents`() {
-        val payload1 =
-            FilePayload(
-                name = "example.pdf",
-                sourceApplicationId = 123L,
-                sourceApplicationInstanceId = "instance-1",
-                type = MediaType.APPLICATION_PDF,
-                encoding = "base64",
-                contents = byteArrayOf(1, 2, 3),
-            )
-        val payload2 =
-            FilePayload(
-                name = "example.pdf",
-                sourceApplicationId = 123L,
-                sourceApplicationInstanceId = "instance-1",
-                type = MediaType.APPLICATION_PDF,
-                encoding = "base64",
-                contents = byteArrayOf(1, 2, 3),
-            )
-
-        assertThat(payload1).isEqualTo(payload2)
-        assertThat(payload1.hashCode()).isEqualTo(payload2.hashCode())
-    }
-
-    @Test
     fun `toString redacts byte array contents`() {
         val payload =
             FilePayload(
